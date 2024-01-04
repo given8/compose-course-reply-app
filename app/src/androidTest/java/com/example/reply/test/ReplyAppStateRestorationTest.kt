ce7a4c3d91becc753791131f2ee3b3982eabd89b
Given Tshuma <given.tshuma@pmconnect.co.uk>
2024-01-04 16:19:04 +0200

    compactScreen config retention test

---
+++ app/src/androidTest/java/com/example/reply/test/ReplyAppStateRestorationTest.kt
@@ -0,0 +1,41 @@
+package com.example.reply.test
+
+import androidx.activity.ComponentActivity
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.StateRestorationTester
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.example.reply.data.local.LocalEmailsDataProvider
+import com.example.reply.ui.ReplyApp
+import org.junit.Rule
+import org.junit.Test
+
+class ReplyAppStateRestorationTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun compactScreen_selectedEmailEmailRetained_afterConfigChange(){
+        val stateRestorationTester = StateRestorationTester(composeTestRule)
+        stateRestorationTester.setContent {
+            ReplyApp(windowSize = WindowWidthSizeClass.Compact)
+        }
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].body)
+        ).assertIsDisplayed()
+
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].subject)).performClick()
+
+        composeTestRule.onNodeWithContentDescriptionForStringId(com.example.reply.R.string.navigation_back).assertExists()
+
+        stateRestorationTester.emulateSavedInstanceStateRestore()
+
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].subject)).assertExists()
+
+        composeTestRule.onNodeWithContentDescriptionForStringId(com.example.reply.R.string.navigation_back).assertExists()
+    }
+}
